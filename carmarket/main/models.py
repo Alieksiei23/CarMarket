6c9eaa1788b0f8dca415fa15383ad75dbfe99269
Aliaksei Sidor <lesha.sidor57@gmail.com>
2025-09-15 17:53:49 +0300

    make models

--- carmarket/main/models.py
+++ carmarket/main/models.py
@@ -12,29 +12,29 @@ class DefaultMixin(models.Model):
 
 
 class Buyer(DefaultMixin):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    balance = models.DecimalField(max_digits=8, decimal_places=2)
 
 
 class Seller(DefaultMixin):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=128)
     description = models.TextField()
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    balance = models.DecimalField(max_digits=8, decimal_places=2)
 
 
 class Showroom(DefaultMixin):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=128)
     location = CountryField()
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    balance = models.DecimalField(max_digits=8, decimal_places=2)
     seller = models.ManyToManyField(Seller, related_name='brands')
 
 
 
 class Car(DefaultMixin):
-    model = models.CharField(max_length=100)
+    model = models.CharField(max_length=128)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE,
                                related_name='seller_id', null=True)
     showroom = models.ForeignKey(Showroom, on_delete=models.CASCADE,
@@ -44,19 +44,19 @@ class Car(DefaultMixin):
 
 class Order(DefaultMixin):
     description = models.TextField()
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    balance = models.DecimalField(max_digits=8, decimal_places=2)
     buyer = models.ForeignKey(Buyer, related_name='buyer',
                               on_delete=models.PROTECT, null=True)
     showroom = models.ForeignKey(Showroom, related_name='showroom',
-                                 on_delete=models.PROTECT, null=True)
+                                 on_delete=models.PROTECT)
     seller = models.ForeignKey(Seller, related_name='seller',
                                on_delete=models.PROTECT, null=True)
 
 
 class Sale(DefaultMixin):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=128)
     description = models.TextField()
-    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    discount = models.DecimalField(max_digits=8, decimal_places=2)
     date_start = models.DateField()
     date_end = models.DateField()
     showroom = models.ForeignKey(Buyer, on_delete=models.CASCADE,
